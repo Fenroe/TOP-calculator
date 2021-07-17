50822a65675cd82b407654df13743e306975e1f6
Fenroe <scrambles9494@gmail.com>
2021-07-17 03:16:46 +0300

    Making progress

--- script/index.js
+++ script/index.js
@@ -1,35 +1,71 @@
+let displayValue = ["0"];
+let storedValue = 0;
+let storedOperator = "";
+
+const calclulatorDisplay = document.querySelector(".display-value");
+const calculatorButtons = document.querySelectorAll("button");
+
 const add = function(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
-}
+};
 
 const subtract = function(firstNumber, secondNumber) {
     return firstNumber - secondNumber;
-}
+};
 
 const multiply = function(firstNumber, secondNumber) {
     return firstNumber * secondNumber;
-}
+};
 
 const divide = function(firstNumber, secondNumber) {
     if(secondNumber === 0) {
         return "NO!";
-    }
+    };
     return firstNumber / secondNumber;
-}
+};
 
 const operate = function(firstNumber, operator, secondNumber) {
     switch(operator) {
         case "+":
             return add(firstNumber, secondNumber);
-            break;
         case "-":
             return subtract(firstNumber, secondNumber);
-            break;
         case "*":
             return multiply(firstNumber, secondNumber);
-            break;
         case "/":
-            return multiply(firstNumber, secondNumber);
-            break; // in the event that more switch statements must be added in the future
+            return divide(firstNumber, secondNumber);     
+    };       
+};
+
+const getDisplayValue = function(input) {
+    if(typeof(calculatorDisplay)==="string") {
+        calclulatorDisplay.split("");
+    };    
+    calclulatorDisplay.push(input);
+    calclulatorDisplay.innerHTML = displayValue.join("");
+}
+
+const calculatorInput = function(input) {
+    switch(input) {
+        case "AC":
+            console.log("clear");
+            break;
+        case "+": case "-": case "*": case "/":
+            console.log("operator");
+            break;
+        default:
+            getDisplayValue(input);
     }
 }
+
+const activateCalculatorValues = function() {    
+    calculatorButtons.forEach(calculatorButton => {
+        calculatorButton.addEventListener("click", () => {
+            calculatorInput(calculatorButton.innerHTML);
+        });
+    });
+};
+
+
+activateCalculatorValues();
+calclulatorDisplay.innerHTML = displayValue;
