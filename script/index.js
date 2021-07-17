aae2dd6d51ed879b6a0b04b2eaeabb837c8155d1
Fenroe <scrambles9494@gmail.com>
2021-07-17 15:22:02 +0300

    Calculator can perform functions once before breaking. It's a start

--- script/index.js
+++ script/index.js
@@ -1,12 +1,13 @@
-let displayValue = ["0"];
+let displayValue = [];
 let storedValue = 0;
 let storedOperator = "";
 
+const defaultDisplayValue = 0;
 const calclulatorDisplay = document.querySelector(".display-value");
 const calculatorButtons = document.querySelectorAll("button");
 
 const add = function(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
+    return firstNumber + secondNumber
 };
 
 const subtract = function(firstNumber, secondNumber) {
@@ -37,35 +38,72 @@ const operate = function(firstNumber, operator, secondNumber) {
     };       
 };
 
+const resetDisplay = function() {
+    displayValue = [];
+    storedValue = 0;
+    storedOperator = "";
+    calclulatorDisplay.innerHTML = defaultDisplayValue;
+}
+
+const storeValues = function(operator) {
+    if(displayValue === []) {
+        return;
+    } else if(storedOperator != "") {
+        displayValue = parseInt(displayValue, 10);
+        storedValue = operate(storedValue, storedOperator, displayValue);
+        calclulatorDisplay.innerHTML = storedValue;
+        storedOperator = operator;
+    } else {
+        storedOperator = operator;
+        storedValue = parseInt(displayValue, 10);
+        displayValue = [];
+        calclulatorDisplay.innerHTML = defaultDisplayValue;
+    }        
+}
+
 const getDisplayValue = function(input) {
-    if(typeof(displayValue)==="string") {
-        displayValue.split("");
-    };    
-    displayValue.push(input);
-    calclulatorDisplay.innerHTML = displayValue.join("");
+    if(input==="0" && displayValue.length===0) {
+        calclulatorDisplay.innerHTML = defaultDisplayValue;
+    } else {
+        if(typeof displayValue === "string") {
+            displayValue = displayValue.split();
+            console.log(typeof displayValue);
+        }
+        displayValue.push(input);
+        displayValue = displayValue.join("");
+        calclulatorDisplay.innerHTML = displayValue;
+        console.log(displayValue);
+        console.log(typeof(displayValue));
+    }
 }
 
 const calculatorInput = function(input) {
     switch(input) {
         case "AC":
-            console.log("clear");
+            resetDisplay();
             break;
         case "+": case "-": case "*": case "/":
-            console.log("operator");
+            storeValues(input);
+            break;
+        case "=":
+            displayValue = parseInt(displayValue, 10);
+            storedValue = operate(storedValue, storedOperator, displayValue);
+            calclulatorDisplay.innerHTML = storedValue;
+            storedOperator = "";
             break;
         default:
             getDisplayValue(input);
-    }
-}
+    };
+};
 
 const activateCalculatorValues = function() {    
     calculatorButtons.forEach(calculatorButton => {
         calculatorButton.addEventListener("click", () => {
-            calculatorInput(calculatorButton.innerHTML);
+            calculatorInput(calculatorButton.innerHTML);            
         });
     });
 };
 
 
 activateCalculatorValues();
-calclulatorDisplay.innerHTML = displayValue;
+resetDisplay();
